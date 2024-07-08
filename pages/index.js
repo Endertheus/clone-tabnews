481adc7ffd5677aed25b84e0a7991684ed0ae173
Endertheus <167995491+Endertheus@users.noreply.github.com>
2024-07-08 13:14:01 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>Olá, como você está hoje? :D</h1>
+  return <h1>Olá, como você está hoje? :D</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
