24fe3c5368e0d718254a6305651f07ecf3a27f11
Endertheus <167995491+Endertheus@users.noreply.github.com>
2024-08-01 17:55:24 +0000

    Altera o código do index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>:O</h1>;
+  return <h1>:O !</h1>;
   <img
     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1xQlw0yzqEFcp-zE1NGbkjaFvlCangvyOMdADn1KT3jENanAA4UKwECOEfcKhzYU"
     alt="Descrição da Imagem"
