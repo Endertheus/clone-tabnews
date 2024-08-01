b3156744f34bc1250fc98a949bd2f4873140d27c
Endertheus <167995491+Endertheus@users.noreply.github.com>
2024-08-01 17:47:06 +0000

    Altera o códig do index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Olá, como você está hoje? :D</h1>;
+  return <h1>:O</h1>;
+  <img
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1xQlw0yzqEFcp-zE1NGbkjaFvlCangvyOMdADn1KT3jENanAA4UKwECOEfcKhzYUZuTc&usqp=CAU"
+    alt="sapo"
+  />;
 }
 
 export default Home;
