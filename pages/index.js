54a0524ca77ed720d7402ca38f8ceec46eea53c3
Endertheus <167995491+Endertheus@users.noreply.github.com>
2024-08-01 17:45:07 +0000

    Adiciona uma imagem no index

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Olá, como você está hoje? :D</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
 export default Home;
