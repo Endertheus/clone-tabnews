85e601912b90d24c9294f359bc1f01f09b5750ca
Endertheus <167995491+Endertheus@users.noreply.github.com>
2024-08-19 15:42:24 +0000

    add `api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
   return <h1>:O !</h1>;
-  <img
-    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1xQlw0yzqEFcp-zE1NGbkjaFvlCangvyOMdADn1KT3jENanAA4UKwECOEfcKhzYU"
-    alt="Descrição da Imagem"
-    width="500"
-  ></img>;
 }
 
 export default Home;
