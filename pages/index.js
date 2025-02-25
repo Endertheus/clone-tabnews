eeea6cf37f09ee2cd634f832b7f00f583a2d69c9
Endertheus <167995491+Endertheus@users.noreply.github.com>
2025-02-25 14:11:41 +0000

    makes database.js client.connect() more robust to errors

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return <h1>Bem-vindo!</h1>;
-  <p>No momento estou em construção, volte em breve</p>;
-  <p>AEUGH</p>;
+  return <p>No momento estou em construção, volte em breve</p>;
+  return <p>AEUGH</p>;
 }
 
 export default Home;
