86e2f537fc97d442b3fff10e361b9e60e741b320
Endertheus <167995491+Endertheus@users.noreply.github.com>
2025-03-12 14:57:22 +0000

    index change

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home() {
-  return <h1>Bem-vindo!</h1>;
-  return <p>No momento estou em construção, volte em breve</p>;
-  return <p>AEUGH</p>;
+  <h1>Bem-vindo!</h1>;
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Minecraft-creeper-face.jpg/480px-Minecraft-creeper-face.jpg"></img>;
 }
 
 export default Home;
