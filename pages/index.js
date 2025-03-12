d80649d8cc375099e9cebc7f3e30a3a2e1c223eb
Endertheus <167995491+Endertheus@users.noreply.github.com>
2025-03-12 14:59:35 +0000

    change index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-  <h1>Bem-vindo!</h1>;
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Minecraft-creeper-face.jpg/480px-Minecraft-creeper-face.jpg"></img>;
+  return <h1>Bem-vindo!</h1>;
 }
 
 export default Home;
